8f3c23cd89d58c38967d898df0354dda9240af1b
Kelvin Magalhaes <117312545+Sonecacps@users.noreply.github.com>
2023-12-01 00:16:04 -0300

    Desafio do rank
    Desafio de função que soma as vitórias e informa o rank que está

---
+++ script2.js
@@ -0,0 +1,28 @@
+
+function partidas (a, b){
+let partidas = a - b;
+return partidas;
+};
+
+let ganhos = partidas (215, 40);
+
+if(ganhos <= 10){
+    rank = "Ferro"
+}else if(ganhos >= 11 && ganhos <= 20){
+    rank = "Bronze"
+}else if(ganhos >= 21 && ganhos <= 50){
+    rank =  "Prata"
+}else if(ganhos >= 51 && ganhos <= 80){
+    rank = "Ouro"
+}else if(ganhos >= 81 && ganhos <= 90){
+    rank =  "Diamante"
+}else if(ganhos >= 91 && ganhos <= 100){
+    rank = "Lendario"
+}else if(ganhos >= 101){
+    rank = "Imortal"  
+}else{
+    "Você não tem ranking ainda"
+    
+}
+
+console.log("O herói tem um saldo de " + ganhos + " está no nivel de " + rank);
